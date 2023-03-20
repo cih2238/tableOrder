24f589c137c83c7e306da5440a916ee01c951bc2
alos2 <alos2@DESKTOP-D14HB9J>
2023-03-20 11:27:11 +0900

    html추가 getMapping추가

--- tableOrder/src/main/java/com/study/tableOrder/controller/MainController.java
+++ tableOrder/src/main/java/com/study/tableOrder/controller/MainController.java
@@ -5,4 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+	@GetMapping("/")
+	public String goMain() {
+		return "/main";
+	}
+
+	@GetMapping("/chatView")
+	public String goChat() {
+		return "/chatView";
+	}
+
+	@GetMapping("/order")
+	public String goOrder() {
+		return "/order";
+	}
+
 }
