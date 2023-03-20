2b4e20d258baa527d5feb56a9ed55f19093a8584
alos2 <alos2@DESKTOP-D14HB9J>
2023-03-20 14:50:39 +0900

    테이블 번호 기능추가

--- tableOrder/src/main/java/com/study/tableOrder/controller/MainController.java
+++ tableOrder/src/main/java/com/study/tableOrder/controller/MainController.java
@@ -1,13 +1,18 @@
 package com.study.tableOrder.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
 	@GetMapping("/")
-	public String goMain() {
+	public String goMain(@RequestParam(value="tableNumber", required=false) Integer tableNumber, Model model) {
+		if(tableNumber != null) {
+			model.addAttribute("tableNumber", tableNumber);
+		}
 		return "/main";
 	}
 
