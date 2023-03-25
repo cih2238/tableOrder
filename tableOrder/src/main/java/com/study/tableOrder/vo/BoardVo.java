cc295fd6c56cf5679baddcf16cd6d7cabda21653
alos1 <alos1@DESKTOP-BC60NS5>
2023-03-25 20:55:31 +0900

    vo get,set,toString 추가

--- tableOrder/src/main/java/com/study/tableOrder/vo/BoardVo.java
+++ tableOrder/src/main/java/com/study/tableOrder/vo/BoardVo.java
@@ -2,10 +2,40 @@ package com.study.tableOrder.vo;
 
 import lombok.Data;
 
-@Data
 public class BoardVo {
 	private int no;
 	private String image;
 	private String content;
 	private String title;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	@Override
+	public String toString() {
+		return "BoardVo [no=" + no + ", image=" + image + ", content=" + content + ", title=" + title + "]";
+	}
+	
 }
